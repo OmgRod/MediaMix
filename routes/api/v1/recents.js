585d82c6b4da4e970acfc47a90f426c2cc1c9ae4
0mgRod <rodrigofonseca20@outlook.com>
2023-11-12 10:05:33 +0000

    recents category (broken) + fixed watch video sizes

--- routes/api/v1/recents.js
+++ routes/api/v1/recents.js
@@ -8,6 +8,9 @@ const router = express.Router();
 // Define a route for fetching recent videos
 router.get('/api/v1/recentVideos', async (req, res) => {
     try {
+        // Extract the 'limit' query parameter from the request
+        const limit = req.query.limit ? parseInt(req.query.limit) : 3; // Default to 3 if 'limit' is not provided or not a valid number
+
         // Create a MySQL connection pool using environment variables
         const pool = mysql.createPool({
             host: process.env.DB_HOSTNAME,
@@ -19,9 +22,9 @@ router.get('/api/v1/recentVideos', async (req, res) => {
         // Attempt to connect to the database
         const connection = await pool.getConnection();
 
-        // Query the database to get the 10 most recent videos
+        // Query the database to get the specified number of most recent videos
         const [rows] = await connection.query(
-            'SELECT * FROM videos ORDER BY CreatedAt DESC LIMIT 3'
+            `SELECT * FROM videos ORDER BY CreatedAt DESC LIMIT ${limit}`
         );
 
         // Release the database connection
